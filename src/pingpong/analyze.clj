d54b045097154fa0eceae7459e673edb2b8c532e
Timo Westkämper <timo.westkamper@mysema.com>
2012-09-25 11:04:24 +0300

    modifications to analyze

--- src/pingpong/analyze.clj
+++ src/pingpong/analyze.clj
@@ -1,35 +1,57 @@
 (ns pingpong.analyze
-  (require [pingpong.calc :as calc]))
+  (:use clojure.pprint)
+  (:import java.io.File)
+  (:require [pingpong.calc :as calc]))
 
-(defn loadgame [file] 
+(defn loadgame 
+  [file] 
   (drop 2 (read-string (slurp file))))
 
-(defn bx [m] (get-in m [:ball :pos :x]))
-(defn by [m] (get-in m [:ball :pos :y]))
-(defn ly [m] (get-in m [:left :y]))
-(defn ry [m] (get-in m [:right :y]))
+(def paddle-height 50)
+(def paddle-width  10)
+(def ball-radius   5)
 
-;TODO Check is the padhit right to calc like this?
-(defn x-y-padhit [game] 
-  (map (juxt bx by #(- (by %) (ly %))) game))
+(defn x-y-paddle 
+  [{:keys [ball left conf]}]
+  [(-> ball :pos :x)
+   (-> ball :pos :y)
+   (-> left :y)]) 
 
-(defn split-chunks [n s]
-  (when (and (seq s) (> (count s) (dec n)))
+(defn split-chunks 
+  [n s]
+  (when 
+    (and (seq s) (> (count s) (dec n)))
     (cons (take n s) (split-chunks n (rest s)))))
 
 (defn turnpoint? 
-  [[x1 x2 x3 x4 :as pts]]
-  (and (> x1 x2) (> x1 x3) (> x4 x3) (> x4 x2) pts))
+  [[x1 x2 x3 x4]]
+  (and x1 x2 x3 x4 (> x1 x2) (> x4 x3)))
 
-(defn turnpoints [game]
+(defn turnpoints 
+  [game]
   "Returns four [x y paddlehit] positions for each left player turnpoints"
-  (filter #(turnpoint? (map first %)) (split-chunks 4 (x-y-padhit game))))
+  (filter 
+    #(turnpoint? (map first %)) 
+    (split-chunks 4 (map x-y-paddle game))))
 
-(defn turnangles [game]
+(defn turnangle
+  [[p1 p2 p3 p4]]
+  (let [coming      (calc/calculate-angle p2 p1)
+        going       (calc/calculate-angle p4 p3)
+        x-at-paddle (+ paddle-width ball-radius)
+        y-at-paddle (calc/calculate-y-at-x coming p2 x-at-paddle)
+        paddle-pos  (/ (+ (last p2) (last p3)) 2)
+        paddle-ctr  (+ paddle-pos (/ paddle-height 2))]
+    [coming going (- y-at-paddle paddle-ctr)]))
+
+(defn turnangles 
   "Returns coming and leaving angles and averaged paddle hitpoint on each left player hit"
-  (map 
-    (fn [[p1 p2 p3 p4]]
-      [(calc/calculate-angle p1 p2)
-       (calc/calculate-angle p2 p4)
-       (/ (+ (last p2) (last p3)) 2)]) 
-    (turnpoints game)))
+  [game]
+  (pprint (map turnangle (turnpoints game))))
+
+(defn dump-results 
+  []
+  (doseq [log (.listFiles (java.io.File. "log"))]
+    (let [game (loadgame (.. log getAbsolutePath))]
+      (turnangles game))))
+        
